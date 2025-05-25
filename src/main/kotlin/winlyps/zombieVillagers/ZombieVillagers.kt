b7a1a4f150c14e3e509e680cc293ddd6788a37ac
Winlypse <nts3@o2.pl>
2025-05-25 21:49:40 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/zombieVillagers/ZombieVillagers.kt
@@ -0,0 +1,14 @@
+package winlyps.zombieVillagers
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ZombieVillagers : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
